95f115b7214c4060fe37b181a61f5de5a55143c1
ml2439 <ml2439@cornell.edu>
2020-05-25 11:35:13 -0400

    Add gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,53 @@
+require("dotenv").config()
+
+module.exports = {
+  siteMetadata: {
+    title: `Meeting Killer Life Saver`,
+    description: `Say bye to overtime meetings.`,
+    author: `ML`,
+    url: `https://todo.com`,
+  },
+  plugins: [
+    `gatsby-plugin-emotion`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Meeting Killer Life Saver",
+        short_name: "MeetingKiller",
+        start_url: "/",
+        background_color: "#55f",
+        theme_color: "#55f",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/favicon-32x32.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+          appId: process.env.FIREBASE_APP_ID,
+          measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+        },
+      },
+    },
+  ],
+}
